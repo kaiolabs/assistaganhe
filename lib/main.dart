7191fe74aed4ccfbca3dd6cbf4a6fa080648a17c
kaiolabs <peypro1@gmail.com>
2023-01-14 13:20:22 -0300

    Adicionando o nome do app

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Infinity Studios ∞',
       theme: ThemeData(primarySwatch: Colors.blue),
       debugShowCheckedModeBanner: false,
       home: const MyHomePage(),
