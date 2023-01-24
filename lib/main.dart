4b81a679aaea3e977a4627c1ee6f5646b1d9fd18
kaiolabs <peypro1@gmail.com>
2023-01-24 14:29:18 -0300

    [refactor] - Editando os textos da tela de home

--- lib/main.dart
+++ lib/main.dart
@@ -71,123 +71,129 @@ class _MyHomePageState extends State<MyHomePage> {
     return Scaffold(
       backgroundColor: ColorOutlet.secondaryColor,
       body: SafeArea(
-        child: Column(
-          children: [
-            Padding(
-              padding: EdgeInsets.only(top: MediaQuery.of(context).size.height * 0.03),
-              child: SizedBox(
-                width: MediaQuery.of(context).size.width * 0.85,
-                child: Row(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Image.asset(
-                      'assets/images/coin.png',
-                      width: 50,
-                      height: 50,
-                    ),
-                    Text(
-                      !boasVindas ? 'Recuperar senha' : 'Infinity Studios ∞',
-                      style: const TextStyle(
-                        color: ColorOutlet.colorWhite,
-                        fontSize: 20,
-                        fontFamily: FontFamilyOutlet.defaultFontFamilyLight,
+        child: SingleChildScrollView(
+          physics: const BouncingScrollPhysics(),
+          child: Column(
+            children: [
+              Padding(
+                padding: EdgeInsets.only(top: MediaQuery.of(context).size.height * 0.03),
+                child: SizedBox(
+                  width: MediaQuery.of(context).size.width * 0.85,
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Image.asset(
+                        'assets/images/coin.png',
+                        width: 50,
+                        height: 50,
                       ),
-                    ),
-                    Container(
-                      width: 20,
-                    )
-                  ],
+                      Text(
+                        !boasVindas ? 'Assista e ganhe' : 'Infinity Studios ∞',
+                        style: const TextStyle(
+                          color: ColorOutlet.colorWhite,
+                          fontSize: 20,
+                          fontFamily: FontFamilyOutlet.defaultFontFamilyLight,
+                        ),
+                      ),
+                      Container(
+                        width: 20,
+                      )
+                    ],
+                  ),
                 ),
               ),
-            ),
-            !boasVindas
-                ? Expanded(
-                    child: Center(
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: <Widget>[
-                          SizedBox(
-                            width: MediaQuery.of(context).size.width * 0.85,
-                            child: const Text(
-                              'Copie o código abaixo, entre no aplicativo e cole no campo de código de confirmação',
-                              style: TextStyle(
-                                color: ColorOutlet.colorWhite,
-                                fontSize: 14,
-                                fontFamily: FontFamilyOutlet.defaultFontFamilyLight,
+              !boasVindas
+                  ? SizedBox(
+                      height: MediaQuery.of(context).size.height * 0.8,
+                      child: Center(
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: <Widget>[
+                            SizedBox(
+                              width: MediaQuery.of(context).size.width * 0.85,
+                              child: const Text(
+                                'Seja bem-vindo, agora você pode acessar o aplicativo e começar a aproveitar os benefícios de fazer parte da nossa comunidade.\n\n' 'O código abaixo é o seu token de acesso, ele é único e não deve ser compartilhado com ninguém. Ele serve para que você possa recuperar sua senha caso a esqueça.',
+                                style: TextStyle(
+                                  color: ColorOutlet.colorWhite,
+                                  fontSize: 14,
+                                  fontFamily: FontFamilyOutlet.defaultFontFamilyLight,
+                                ),
                               ),
                             ),
-                          ),
-                          Container(
-                            constraints: BoxConstraints(maxWidth: MediaQuery.of(context).size.width * 0.85),
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(SizeOutlet.borderRadiusSizeSmall),
-                              border: Border.all(color: ColorOutlet.colorWhite),
-                            ),
-                            padding: const EdgeInsets.symmetric(horizontal: SizeOutlet.paddingSizeSmall, vertical: SizeOutlet.paddingSizeSmall),
-                            margin: const EdgeInsets.symmetric(vertical: SizeOutlet.borderRadiusSizeBig),
-                            child: Text(
-                              tokenController,
-                              style: const TextStyle(
-                                color: ColorOutlet.colorWhite,
-                                fontSize: 12,
-                                fontFamily: FontFamilyOutlet.defaultFontFamilyLight,
+                            Container(
+                              constraints: BoxConstraints(maxWidth: MediaQuery.of(context).size.width * 0.85),
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(SizeOutlet.borderRadiusSizeSmall),
+                                border: Border.all(color: ColorOutlet.colorWhite),
+                              ),
+                              padding: const EdgeInsets.symmetric(horizontal: SizeOutlet.paddingSizeSmall, vertical: SizeOutlet.paddingSizeSmall),
+                              margin: const EdgeInsets.symmetric(vertical: SizeOutlet.borderRadiusSizeBig),
+                              child: Text(
+                                tokenController,
+                                style: const TextStyle(
+                                  color: ColorOutlet.colorWhite,
+                                  fontSize: 12,
+                                  fontFamily: FontFamilyOutlet.defaultFontFamilyLight,
+                                ),
                               ),
                             ),
-                          ),
-                          SizedBox(
-                            width: MediaQuery.of(context).size.width * 0.85,
-                            child: ButtonPattern(
-                              label: 'Copiar código',
-                              onPressed: () {
-                                FlutterClipboard.copy(tokenController).then((value) => print('copied'));
-                                snackBarMessenger(context: context, message: 'Código copiado com sucesso!');
-                              },
+                            SizedBox(
+                              width: MediaQuery.of(context).size.width * 0.85,
+                              child: ButtonPattern(
+                                label: 'Copiar código',
+                                onPressed: () {
+                                  FlutterClipboard.copy(tokenController).then((value) => print('copied'));
+                                  snackBarMessenger(context: context, message: 'Código copiado com sucesso!');
+                                },
+                              ),
                             ),
-                          ),
-                        ],
+                          ],
+                        ),
                       ),
-                    ),
-                  )
-                : Expanded(
-                    child: Center(
-                      child: SizedBox(
-                        width: MediaQuery.of(context).size.width * 0.85,
-                        child: const Text(
-                          'Seja bem-vindo, agora você pode acessar o aplicativo e começar a aproveitar os benefícios de fazer parte da nossa comunidade.',
-                          textAlign: TextAlign.center,
-                          style: TextStyle(
-                            color: ColorOutlet.colorWhite,
-                            fontSize: 14,
-                            fontFamily: FontFamilyOutlet.defaultFontFamilyLight,
+                    )
+                  : SizedBox(
+                      height: MediaQuery.of(context).size.height * 0.8,
+                      child: Center(
+                        child: SizedBox(
+                          width: MediaQuery.of(context).size.width * 0.85,
+                          child: const Text(
+                            'Seja bem-vindo, agora você pode acessar o aplicativo e começar a aproveitar os benefícios de fazer parte da nossa comunidade.',
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              color: ColorOutlet.colorWhite,
+                              fontSize: 14,
+                              fontFamily: FontFamilyOutlet.defaultFontFamilyLight,
+                            ),
                           ),
                         ),
                       ),
                     ),
-                  ),
-            Padding(
-              padding: EdgeInsets.only(bottom: MediaQuery.of(context).size.height * 0.03),
-              child: Column(
-                children: [
-                  SizedBox(
-                    height: 30,
-                    child: Image.asset(
-                      'assets/images/infinidade.png',
-                      fit: BoxFit.contain,
+              Padding(
+                padding: EdgeInsets.only(bottom: MediaQuery.of(context).size.height * 0.03),
+                child: Column(
+                  children: [
+                    SizedBox(
+                      height: 30,
+                      child: Image.asset(
+                        'assets/images/infinidade.png',
+                        fit: BoxFit.contain,
+                      ),
                     ),
-                  ),
-                  const Text(
-                    'Infinity Studios ∞',
-                    style: TextStyle(
-                      fontFamily: FontFamilyOutlet.defaultFontFamilyMedium,
-                      fontSize: SizeOutlet.textSizeMicro1,
-                      color: ColorOutlet.colorPrimaryDark,
+                    const Text(
+                      'Infinity Studios ∞',
+                      style: TextStyle(
+                        fontFamily: FontFamilyOutlet.defaultFontFamilyMedium,
+                        fontSize: SizeOutlet.textSizeMicro1,
+                        color: ColorOutlet.colorPrimaryDark,
+                      ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
